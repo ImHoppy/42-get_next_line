83d39ce8ae1e8ddd142ac4357621a04ec65e3e5f
Marius BRAETS <mbraets@student.42.fr>
2021-12-20 17:58:22 +0100

    first init

---
+++ get_next_line.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbraets <mbraets@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/20 12:37:31 by mbraets           #+#    #+#             */
+/*   Updated: 2021/12/20 17:49:31 by mbraets          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*get_line();
+
+char	*save_line(char *save, char *buffer);
+
+#include <stdio.h>
+char	*get_next_line(int fd)
+{
+	static char	buffer[BUFFER_SIZE + 1];
+	int			count;
+	char		*line;
+	char		*save;
+	int			len;
+	int			index;
+
+	line = "";
+	len = ft_strlen(buffer);
+	count = read(fd, buffer + len, BUFFER_SIZE);
+	if (count == -1)
+		return (NULL);
+	while (count || buffer[0] != '\0')
+	{
+		//printf("buff:%s line:%s save:%s", buffer, line, save);
+		buffer[count] = '\0';
+		index = ft_str_eol(buffer);
+		if (index == -1)
+		{
+			save = malloc(sizeof(char) * (len + count + 1));
+			if (!save)
+				return (NULL);
+			ft_strncpy(save, buffer, len + count);
+			line = ft_strjoin(line, save);
+			count = read(fd, buffer, BUFFER_SIZE);
+			len = 0;
+		}
+		else
+		{
+			save = malloc(sizeof(char) + (index + 2));
+			if (!save)
+				return (NULL);
+			save = ft_strncpy(save, buffer, index + 1);
+			line = ft_strjoin(line, save);
+			ft_strncpy(buffer, buffer + index + 1, BUFFER_SIZE - index - 1);
+			return (line);
+		}
+	}
+	return (line);
+}
+#include <fcntl.h>
+int main(int argc, char **argv)
+{
+	char *s;
+	int fd;
+	if (argc > 1)
+	{
+		fd = open(argv[1], 0);
+		while ((s = get_next_line(fd)) && *s != '\0')
+		{
+			printf("%p: %s", s, s);
+			//free(s);
+		}
+	}
+	else
+	{
+		fd = open("f", 0);
+		s = get_next_line(fd);
+		printf("%s", s);
+		// free(s);
+	}
+	return (0);
+}
---
+++ get_next_line_utils.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbraets <mbraets@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/20 12:35:01 by mbraets           #+#    #+#             */
+/*   Updated: 2021/12/20 17:50:05 by mbraets          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int	ft_strlen(char *s)
+{
+	int	n;
+
+	n = 0;
+	while (s[n])
+		n++;
+	return (n);
+}
+
+int	ft_str_eol(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == '\n')
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+char	*ft_strncpy(char *dst, char *src, int len)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] && i < len)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*result;
+	int		size1;
+	int		size2;
+
+	size1 = ft_strlen(s1);
+	size2 = ft_strlen(s2);
+	result = malloc(sizeof(char) * (size1 + size2));
+	if (!result)
+		return (NULL);
+	ft_strncpy(result, s1, size1);
+	ft_strncpy(result + size1, s2, size2);
+	if (!s1)
+		free(s1);
+	if (s2)
+		free(s2);
+	return (result);
+}
