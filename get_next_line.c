d11d975389ea2ced04bc8fd6226ba9f37441fd1a
Marius BRAETS <mbraets@student.42.fr>
2021-12-21 19:01:49 +0100

    fixing buffer size out range, need tester fix

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mbraets <mbraets@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/20 12:37:31 by mbraets           #+#    #+#             */
-/*   Updated: 2021/12/20 17:49:31 by mbraets          ###   ########.fr       */
+/*   Updated: 2021/12/21 18:59:54 by mbraets          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,10 @@ char	*get_next_line(int fd)
 
 	line = "";
 	len = ft_strlen(buffer);
-	count = read(fd, buffer + len, BUFFER_SIZE);
+	if (fd < 0)
+		return (NULL);
+	count = read(fd, buffer + len, BUFFER_SIZE - len);
+	// printf("%d %d\n", len, count);
 	if (count == -1)
 		return (NULL);
 	while (count || buffer[0] != '\0')
@@ -59,26 +62,26 @@ char	*get_next_line(int fd)
 	}
 	return (line);
 }
-#include <fcntl.h>
-int main(int argc, char **argv)
-{
-	char *s;
-	int fd;
-	if (argc > 1)
-	{
-		fd = open(argv[1], 0);
-		while ((s = get_next_line(fd)) && *s != '\0')
-		{
-			printf("%p: %s", s, s);
-			//free(s);
-		}
-	}
-	else
-	{
-		fd = open("f", 0);
-		s = get_next_line(fd);
-		printf("%s", s);
-		// free(s);
-	}
-	return (0);
-}
+// #include <fcntl.h>
+// int main(int argc, char **argv)
+// {
+// 	char *s;
+// 	int fd;
+// 	if (argc > 1)
+// 	{
+// 		fd = open(argv[1], 0);
+// 		while ((s = get_next_line(fd)) && *s != '\0')
+// 		{
+// 			printf("%p: %s", s, s);
+// 			//free(s);
+// 		}
+// 	}
+// 	else
+// 	{
+// 		fd = open("blanks", 0);
+// 		s = get_next_line(fd);
+// 		printf("%s", s);
+// 		// free(s);
+// 	}
+// 	return (0);
+// }
